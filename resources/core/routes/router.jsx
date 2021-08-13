d2d10235e79a6cdbc0a10b79c9c41d9b11c8aa71
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-08-13 10:44:42 -0300

    feat: add initial example

---
+++ resources/core/routes/router.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <p>Teste</p>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default Router
