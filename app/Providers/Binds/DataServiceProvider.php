0a69a8b71cc120fb050331f978f8b4731afc6180
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-08-13 16:02:56 -0300

    feat: register data layer binds

---
+++ app/Providers/Binds/DataServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers\Binds;
+
+use App\Data\Http\GuzzleHttpClient;
+use App\Data\Interfaces\HttpClient;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use Illuminate\Support\ServiceProvider;
+
+class DataServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(ClientInterface::class, function () {
+            return new Client([
+                'verify' => false,
+                'headers' => [
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                    'Accept' => '*/*',
+                    'Access-Control-Allow-Origin' => 1,
+                ],
+                'timeout' => intval(env('HTTP_CLIENT_REQUEST_TIMEOUT')),
+                'timeout_connect' => intval(env('HTTP_CLIENT_CONNECT_TIMEOUT')),
+            ]);
+        });
+
+        $this->app->bind(HttpClient::class, GuzzleHttpClient::class);
+    }
+}
--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Bind Service Providers...
+         */
+
+        App\Providers\Binds\DataServiceProvider::class
     ],
 
     /*
