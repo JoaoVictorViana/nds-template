cbcdf307036694ffc609d5bf5bdcb6262d675584
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2021-08-21 22:08:41 -0300

    Change application title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Nome do Projeto | Tela</title>
+        <title>NDS Layout</title>
         <!-- Styles -->
         <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
     </head>
