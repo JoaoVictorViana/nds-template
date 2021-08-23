2dedc1cdd3e9be7f93098fe2404517b5b0c9b2fb
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-08-23 10:38:14 -0300

    feat: add BancoCentral contract

---
+++ app/Domain/Interfaces/Apis/BancoCentral.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\Interfaces\Apis;
+
+use App\Domain\Entities\UsuarioEntity;
+
+interface BancoCentral
+{
+    public function loadUsuario(string $id): UsuarioEntity;
+
+    public function loadUsuarioByCredentials(string $identifier, string $password): UsuarioEntity;
+
+    public function checkUsuarioByCredentials(string $identifier, string $password): bool;
+}
