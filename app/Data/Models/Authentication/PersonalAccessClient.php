d232240a2fedde2edb18141764189ef3e1c854e1
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-08-24 14:32:47 -0300

    fix: adjust PersonalAccessClient

--- app/Data/Models/Authentication/PersonalAccessClient.php
+++ app/Data/Models/Authentication/PersonalAccessClient.php
@@ -4,7 +4,7 @@ namespace App\Data\Models\Authentication;
 
 use Laravel\Passport\PersonalAccessClient as PassportPersonalAccessClient;
 
-class Client extends PassportPersonalAccessClient
+class PersonalAccessClient extends PassportPersonalAccessClient
 {
     public $table = 'personal_access_clients';
 }
