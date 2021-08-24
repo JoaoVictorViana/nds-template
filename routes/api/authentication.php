82e90cc7c9d51df2b6244746167dbbd99774c389
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-08-24 16:01:46 -0300

    feat: add logout route

--- routes/api/authentication.php
+++ routes/api/authentication.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Authentication\LoginController;
+use App\Http\Controllers\Authentication\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [LoginController::class, 'handle'])->name('login');
+Route::post('/logout', [LogoutController::class, 'handle'])->name('logout');
