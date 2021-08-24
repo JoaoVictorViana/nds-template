88e6cec53b522925c1f30cfffde5ad0cacb43d8e
Gabriel Cleiton <gcleiton.dev@gmail.com>
2021-08-24 14:33:22 -0300

    feat: add HasApiTokens trait to Usuario model

--- app/Data/Models/Usuario.php
+++ app/Data/Models/Usuario.php
@@ -3,9 +3,12 @@
 namespace App\Data\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class Usuario extends Authenticatable
 {
+    use HasApiTokens;
+
     protected $fillable = [
         'id',
         'nome',
